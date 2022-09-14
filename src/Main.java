5a620cc46f3774c7a9aaad7955bfa3b686313af6
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-14 20:39:17 +0300

    jambo-cod

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        sqrt(10);
+    }
+    public static UnaryOperator<Double> sqrt( int a) {
+        UnaryOperator<Double> num = (n) -> n*n;
+      System.out.println(Math.sqrt(a));
+      return num ;
     }
 }
